6a09e1fe23238e5d1a59d98f7d4dfb92741b7241
Adam Hartz <ahartz1@gmail.com>
2015-10-19 17:22:55 -0400

    Was working; trying to subclass

--- test_weather.py
+++ test_weather.py
@@ -1,12 +1,21 @@
+import os
 import requests_mock
-from weather import *
+from weather import CurrentConditions
+
+my_secret_key = os.environ['WU_KEY']
 
 
 @requests_mock.Mocker()
 def test_current_conditions(m):
-    with open('current.json') as curr_conditions:
-        m.get(fullurl, text=curr_conditions)
+    fullurl = 'http://api.wunderground.com/api/{}/conditions/q/94101'.format(
+        my_secret_key)
 
-        conditions = CurrentConditions('94101')
-        res = conditions.run()
-        assert res == ('expected')
+    with open('current-sf.json') as curr_conditions:
+        m.get(fullurl, text=curr_conditions.read())
+
+    conditions = CurrentConditions('conditions', '94101')
+    res = conditions.run()
+
+    assert res['city_state'] == "San Francisco, CA"
+    assert res['curr_temp'] == 66.3
+    assert res['curr_weather'] == "Partly Cloudy"
--- weather.py
+++ weather.py
@@ -5,19 +5,53 @@ import requests
 my_secret_key = os.environ['WU_KEY']
 
 
-class CurrentConditions(zipcode):
+class APIException(Exception):
+    '''Raised when request status is not 200'''
     pass
 
 
-class TenDay(zipcode):
+class WUndergroundInfo:
+    '''Parent Class for Accessing Weather Underground API'''
+
+    def __init__(self, info_type, q_string):
+        self.info_type = info_type
+        self.q_string = q_string
+
+    def run(self):
+        res = requests.get(
+            'http://api.wunderground.com/api/{}/{}}/q/{}'.format(
+                my_secret_key, self.info_type, self.q_string))
+
+        if res.status_code != 200:
+            raise APIException('Request did not return 200; '
+                               'request limit may have been reached.')
+
+        return res.json()
+
+
+class CurrentConditions(WUndergroundInfo):
+    '''Given zipcode, get full city/state and current temp in ˚F and weather'''
+
+    def run(self):
+        res = super(WUndergroundInfo, self).run()
+
+        city_state = res['current_observation']['display_location']['full']
+        curr_temp = res['current_observation']['temp_f']
+        curr_weather = res['current_observation']['weather']
+
+        return {'city_state': city_state, 'curr_temp': curr_temp,
+                'curr_weather': curr_weather}
+
+
+class TenDay:
     pass
 
 
-class SunriseSunset(zipcode):
+class SunriseSunset:
     pass
 
 
-class WeatherAlerts(zipcode):
+class WeatherAlerts:
     pass
 
 
