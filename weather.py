25a320c31f4d276c3fa22ca818e7d545e1c2ddd4
Adam Hartz <ahartz1@gmail.com>
2015-10-19 13:51:50 -0400

    Add weather.py with skeleton classes

---
+++ weather.py
@@ -0,0 +1,20 @@
+import requests
+
+
+class CurrentConditions(zipcode):
+    pass
+
+
+class TenDay(zipcode):
+    pass
+
+
+class SunriseSunset(zipcode):
+    pass
+
+
+class WeatherAlerts(zipcode):
+    pass
+
+class ActiveHurricanes:
+    pass
